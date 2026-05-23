17cde0421fdbd9aacc4cb21e3723a95e89183fda
nithin <knithinkumar29@gmail.com>
2026-05-24 00:19:43 +0530

    feature of natural numbers

---
+++ balu.c
@@ -0,0 +1,10 @@
+//print first 10 natural numbers
+#include <stdio.h>      
+int main() {    
+    int i;    
+    printf("First 10 natural numbers are: \n");    
+    for(i=1; i<=10; i++) {    
+        printf("%d ", i);    
+    }    
+    return 0;  
+}
